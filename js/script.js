933aa2c838f41b3409399303edb161f76a1870df
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2021-12-14 12:37:20 +0100

    snack 6 function

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Scrivere una funzione (e lanciarla :razzo:) che 
+// accetti tre argomenti, un array e due numeri
+//  (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori 
+// che hanno la posizione compresa tra i due numeri.
+// Usiamo i nuovi metodi degli array foreach o filter.
+
+
+
+
+
+
+function arrayNumber () {
+    let array1 = [2,3,4,5,6];
+    let a = 2;
+    let b = 6;
+    let array3 = [];
+    let filtro = array1.filter(numero => numero > a && numero < b);
+    array3.push(filtro);
+    return array3
+};
+
+let print = arrayNumber();
+
+console.log(print);
